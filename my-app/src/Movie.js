e600014877a263d0616cbd2f55d8a78e5d2d6848
소상훈 <sshlc940503@naver.com>
2019-01-20 17:17:46 +0900

    Movie.js 컴포넌트 수정
    Smart Component => Dumb Component

--- my-app/src/Movie.js
+++ my-app/src/Movie.js
@@ -1,32 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Movie.css";
 import PropTypes from "prop-types";
-class Movie extends Component {
 
-  //props 데이터 타입 검사
-  static propTypes ={
-    title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
-  }
-
-  render() {
-    return (
-      <div>
-        <MoviePoster poster={this.props.poster} />
-        <h1>{this.props.title}</h1>
-      </div>
-    );
-  }
+function Movie({title,poster}){
+  return(
+    <div>
+      <MoviePoster poster = {poster}/> 
+      <h1> {title} </h1> 
+    </div>
+  )
+}
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
 }
 
-class MoviePoster extends Component {
-
-  static propTypes ={
-    poster: PropTypes.string.isRequired
-  }
-  render() {
-    return <img src={this.props.poster} />;
-  }
+function MoviePoster({poster}) {
+  return ( 
+    <img src = {poster} alt = "Movie Poster" />
+  )
 }
 
+MoviePoster.propTypes = {
+  poster: PropTypes.string.isRequired
+}
 export default Movie;
